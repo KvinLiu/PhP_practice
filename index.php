7a46458b9a1c477e7dbbb76bd0ff04b3fffcd16f
Kevin Liu <kevin07liu@gmail.com>
2017-08-25 21:16:30 +0800

    do a little of refactoring

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 
 require 'Task.php';
+require 'functions.php';
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
+$pdo = connectToDb();
 
-$statement = $pdo->prepare('select * from todos');
-$statement->execute();
-
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
